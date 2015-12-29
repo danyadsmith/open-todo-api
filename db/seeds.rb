a3027e2fcac41c023d5d3e31deb81b8b5c990078
Danya D. Smith <danyadsmith@gmail.com>
2015-12-29 18:52:52 -0500

    Seed the Database with Test Data
    Here we update the seeds.rb file in order to seed the database with 10
    test users, 50 lists, and 250 list items.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,49 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# ##############################################################
+# Create Users (10)
+# ##############################################################
+
+9.times do
+  user = User.create!(
+    username: Faker::Internet.user_name,
+    password_digest: "opentodoapi"
+  )
+end
+
+my_account = User.create!(
+  username: "danyadsmith",
+  password_digest: "opentodoapi"
+)
+
+users = User.all
+
+# ##############################################################
+# Create Lists (50)
+# ##############################################################
+
+50.times do
+  list = List.create!(
+    title: Faker::Lorem.sentence,
+    list_type: Faker::Number.between(0, 5),
+    user: users.sample
+  )
+  list.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+end
+
+lists = List.all
+
+# ##############################################################
+# Create Items (250)
+# ##############################################################
+
+250.times do
+  item = Item.create!(
+    title: Faker::Lorem.sentence,
+    priority: Faker::Number.between(0, 3),
+    percent_complete: Faker::Number.between(0, 100),
+    notes: Faker::Lorem.paragraph(1),
+    today: Faker::Number.between(0,1),
+    list: lists.sample
+  )
+  item.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+end
+
